672296e0026619e1c5b2cb419ca8be9095142d23
PedroMartinsexia <33548914+PedroMartinsexia@users.noreply.github.com>
2018-05-29 16:23:17 +0200

    view pedro

--- Lorann/view/src/main/java/view/Panel.java
+++ Lorann/view/src/main/java/view/Panel.java
@@ -1,29 +1,42 @@
-package view;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import java.awt.*;
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class Panel extends JPanel {
-	public Window f ;
-	public int sizeCase = 32;
-	public Panel (Window f) {
-		this.f= f;	
-		//repaint();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Fenetre f;
+	private int sizeCase = 32;
+	public Panel(Fenetre f) {
+		this.f = f;
 	}
-	
+
 	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
 		int x = 0, y = 0;
 		g.setColor(Color.WHITE);
-		for(char[] tc : f.m.lvl1.caractere) {
-			for(char c : tc) {
-				
+		for (char[] tc : f.m.lvl1.caractere) {
+			for (char c : tc) {
+
 				Image img = f.m.getImage(c);
-				g.drawImage(img, x*sizeCase, 20+y*sizeCase, null);
-				//g.drawString(c+"", x*sizeCase, 20+y*sizeCase);
+				g.drawImage(img, x * sizeCase, 20 + y * sizeCase, null);
+				// g.drawString(c+"", x*sizeCase, 20+y*sizeCase);
 				y++;
 			}
 			x++;
-			y=0;
+			y = 0;
 		}
+		Action perso = new Action(g);
+
 	}
 }
--- Lorann/view/src/main/java/view/Window.java
+++ Lorann/view/src/main/java/view/Window.java
@@ -1,34 +1,65 @@
-package view;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-import model.Transf;
-
-public class Window extends JFrame{
+public class Fenetre extends JFrame implements KeyListener{
 	
-	
-	Transf m = new Transf();
-	//methode pour transformer char en images 
+	public Model m = new Model();
+	private Panel pan;
+	private Action perso;
 	public Fenetre() {
 		 
 		
 		
 	    this.setTitle("Lorann");
-	    this.setSize (800, 800);
+	    this.setSize (670, 430);
 	    this.setLocationRelativeTo(null);
 	    this.setBackground(Color.BLACK);        
-	    
-	    
-	    
-	    this.setContentPane(new Panel(this));               
+	    pan = new Panel(this);
+	    this.setContentPane(pan);               
 	    this.setVisible(true);
 		
+		this.addKeyListener(this);
 		
 		
 		
 		
 		
-		
-	} 
+	}
 
+	@Override
+	public void keyPressed(KeyEvent evt) {
+
+		if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
+			perso.setZ(perso.getZ()-1);
+		}
+
+		if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
+			perso.setZ(perso.getZ() + 1);
+		}
+
+		if (evt.getKeyCode() == KeyEvent.VK_UP) {
+			perso.setW(perso.getW() - 1);
+		}
+
+		if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
+			perso.setW(perso.getW() + 1);
+		}
+		pan.repaint();
+	}
+	
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	} 	
 }
